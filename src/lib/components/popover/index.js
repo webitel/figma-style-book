124ffe0033722bee2016a1bd83ad2cc1494b9e48
Олександр Пальонний <s2567k@gmail.com>
2025-06-05 11:08:40 +0300

    feat: added primeuix components colors/sizes settings [WTEL-6985](https://webitel.atlassian.net/browse/WTEL-6985)

---
+++ src/lib/components/popover/index.js
@@ -0,0 +1,13 @@
+export const PopoverSizesScheme = {
+  padding: '{overlay.popover.padding}'
+};
+
+export const PopoverColorsScheme = {
+  background: '{overlay.popover.background}',
+  borderColor: '{overlay.popover.border.color}',
+  color: '{overlay.popover.color}',
+  borderRadius: '{overlay.popover.border.radius}',
+  shadow: '{overlay.popover.shadow}',
+  gutter: '10px',
+  arrowOffset: '1.25rem'
+};

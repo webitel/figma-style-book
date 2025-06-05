d366e9e2b468736557176dc467113b047bf49e14
Олександр Пальонний <s2567k@gmail.com>
2025-06-05 11:09:54 +0300

    feat: update tooltip schemes [WTEL-6985](https://webitel.atlassian.net/browse/WTEL-6985)

--- src/lib/components/tooltip/index.js
+++ src/lib/components/tooltip/index.js
@@ -1,9 +1,12 @@
 export const TooltipColorsScheme = {
   background: '{surface.600}',
   color: '{surface.0}',
+  shadow: '{overlay.popover.shadow}',
+};
+
+export const TooltipSizesScheme = {
   maxWidth: '12.5rem',
   gutter: '0.25rem',
-  shadow: '{overlay.popover.shadow}',
   padding: '0.5rem 0.75rem',
   borderRadius: '{overlay.popover.border.radius}'
 };

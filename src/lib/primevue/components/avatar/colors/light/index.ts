0cd5e0373728188d575fb778f7c6ca6e6e746623
Олександр Пальонний <s2567k@gmail.com>
2025-07-08 18:53:58 +0300

    feat: add color and size schemes for Avatar, Menubar, and Toolbar components [WTEL-6985](https://webitel.atlassian.net/browse/WTEL-6985)

---
+++ src/lib/primevue/components/avatar/colors/light/index.ts
@@ -0,0 +1,21 @@
+export default {
+  root: {
+    background: '{content.border.color}',
+    color: '{content.color}',
+  },
+  letters: {
+    'p1-color': '{blue.300}',
+    'p2-color': '{blue.300}',
+    'p3-color': '{blue.300}',
+    'p4-color': '{blue.300}',
+    'p5-color': '{blue.300}',
+    'p6-color': '{blue.300}',
+    'p7-color': '{blue.300}',
+    'p8-color': '{blue.300}',
+    'p9-color': '{blue.300}',
+    'p10-color': '{blue.300}',
+    'p11-color': '{blue.300}',
+    'p12-color': '{blue.300}',
+    'p13-color': '{blue.300}',
+  }
+}

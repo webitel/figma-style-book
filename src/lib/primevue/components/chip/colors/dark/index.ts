2ad88ddebd6eb4f0c9671f0daebedbbc2e291c93
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-08-05 10:43:41 +0300

    feat: add iconColor property to chip remove icon color configuration [WTEL-7278](https://webitel.atlassian.net/browse/WTEL-7278)

--- src/lib/primevue/components/chip/colors/dark/index.ts
+++ src/lib/primevue/components/chip/colors/dark/index.ts
@@ -1,43 +1,43 @@
 const darkColors = {
   background: '{surface.100}',
   color: '{surface.800}',
-  /**
-   * @author Yehor Hlukhov
-   * 
-   * referred to Evgeniy Trahtenberg
-   * need to add colors for main
-   * */
   main: {
     background: '{amber.650}',
-    color: '{amber.950}'
+    color: '{amber.950}',
+    iconColor: '{red.500}'
   },
   primary: {
     background: '{primary.color}',
-    color: '{primary.foreground}'
+    color: '{primary.foreground}',
+    iconColor: '{red.500}'
   },
   secondary: {
     background: '{secondary.color}',
-    color: '{secondary.foreground}'
+    color: '{secondary.foreground}',
+    iconColor: '{red.500}'
   },
   success: {
     background: '{success.color}',
-    color: '{success.foreground}'
+    color: '{success.foreground}',
+    iconColor: '{red.500}'
   },
   warning: {
     background: '{warn.color}',
-    color: '{warn.foreground}'
+    color: '{warn.foreground}',
+    iconColor: '{red.500}'
   },
   error: {
     background: '{error.color}',
-    color: '{error.foreground}'
+    color: '{error.foreground}',
+    iconColor: '{red.500}'
   },
   transfer: {
     background: '{transfer.color}',
-    color: '{transfer.foreground}'
+    color: '{transfer.foreground}',
+    iconColor: '{red.500}'
   },
   removeIcon: {
     focusRing: {
-      color: '{focus.ring.color}',
       shadow: '{form.field.focus.ring.shadow}'
     }
   }

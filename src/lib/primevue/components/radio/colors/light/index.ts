6cd311826a38fb61af9ca504075def29a41d63fd
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-08-06 16:13:27 +0300

    feat: adding radio button from primevue [WTEL-7363](https://webitel.atlassian.net/browse/WTEL-7363)

---
+++ src/lib/primevue/components/radio/colors/light/index.ts
@@ -0,0 +1,29 @@
+const lightColors = {
+  root: {
+    background: '{form.field.background}',
+    checkedBackground: '{primary.color}',
+    checkedHoverBackground: '{primary.hover.color}',
+    disabledBackground: '{form.field.disabled.background}',
+    filledBackground: '{form.field.filled.background}',
+    borderColor: '{form.field.border.color}',
+    hoverBorderColor: '{form.field.hover.border.color}',
+    focusBorderColor: '{form.field.border.color}',
+    checkedBorderColor: '{primary.color}',
+    checkedHoverBorderColor: '{primary.hover.color}',
+    checkedFocusBorderColor: '{primary.color}',
+    checkedDisabledBorderColor: '{form.field.border.color}',
+    invalidBorderColor: '{form.field.invalid.border.color}',
+    shadow: '{form.field.shadow}',
+    focusRing: {
+      color: '{focus.ring.color}',
+      shadow: '{focus.ring.shadow}'
+    }
+  },
+  icon: {
+    checkedColor: '{primary.contrast.color}',
+    checkedHoverColor: '{primary.contrast.color}',
+    disabledColor: '{form.field.disabled.color}',
+  }
+}
+
+export default lightColors
---
+++ src/lib/primevue/components/radio/sizes/index.ts
@@ -0,0 +1,31 @@
+const sizes = {
+  root: {
+    width: '1.25rem',
+    height: '1.25rem',
+    focusRing: {
+      width: '{focus.ring.width}',
+      style: '{focus.ring.style}',
+      offset: '{focus.ring.offset}',
+    },
+    transitionDuration: '{form.field.transition.duration}',
+    sm: {
+        width: '1rem',
+        height: '1rem'
+    },
+    lg: {
+        width: '1.5rem',
+        height: '1.5rem'
+  }
+  },
+  icon: {
+    size: '0.75rem',
+    sm: {
+      size: '0.5rem'
+    },
+    lg: {
+        size: '1rem'
+    }
+  }
+}
+
+export default sizes

961e4a3236ba02d7dcdad636a3dfc817deb7591c
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-08-06 16:46:13 +0300

    fix: enhance chip remove icon configuration [WTEL-7278](https://webitel.atlassian.net/browse/WTEL-7278)

--- src/lib/primevue/components/chip/colors/light/index.ts
+++ src/lib/primevue/components/chip/colors/light/index.ts
@@ -4,43 +4,49 @@ const lightColors = {
   main: {
     background: '{amber.200}',
     color: '{amber.950}',
-    iconColor: '{red.500}'
+    iconColor: '{red.500}',
+    iconHoverColor: '{red.100}'
   },
   primary: {
     background: '{primary.color}',
     color: '{primary.foreground}',
-    iconColor: '{red.500}'
+    iconColor: '{red.500}',
+    iconHoverColor: '{red.100}'
   },
   secondary: {
     background: '{secondary.color}',
     color: '{secondary.foreground}',
-    iconColor: '{red.500}'
+    iconColor: '{red.500}',
+    iconHoverColor: '{red.100}'
   },
   success: {
     background: '{success.color}',
     color: '{success.foreground}',
-    iconColor: '{red.500}'
+    iconColor: '{red.500}',
+    iconHoverColor: '{red.100}'
   },
   warning: {
     background: '{warn.color}',
     color: '{warn.foreground}',
-    iconColor: '{red.500}'
+    iconColor: '{red.500}',
+    iconHoverColor: '{red.100}'
   },
   error: {
     background: '{error.color}',
     color: '{error.foreground}',
-    iconColor: '{red.500}'
+    iconColor: '{red.500}',
+    iconHoverColor: '{red.100}'
   },
   transfer: {
     background: '{transfer.color}',
     color: '{transfer.foreground}',
-    iconColor: '{red.500}'
+    iconColor: '{red.500}',
+    iconHoverColor: '{red.100}'
   },
   removeIcon: {
     focusRing: {
       shadow: '{form.field.focus.ring.shadow}'
     },
-    hoverColor: '{red.100}'
   }
 }
 

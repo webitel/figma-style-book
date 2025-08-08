8b01586bad4e2ed05697474e9ed62f321ae54836
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-08-08 12:12:02 +0300

    feat: adding divider from primevue [WTEL-7375](https://webitel.atlassian.net/browse/WTEL-7375)

---
+++ src/lib/primevue/components/divider/sizes/index.ts
@@ -0,0 +1,18 @@
+const sizes = {
+  horizontal: {
+    margin: '1rem 0',
+    padding: '0 1rem',
+    content: {
+        padding: '0 0.5rem'
+    }
+  },
+  vertical: {
+    margin: '0 1rem',
+    padding: '0.5rem 0',
+    content: {
+        padding: '0.5rem 0'
+    }
+  }
+}
+
+export default sizes

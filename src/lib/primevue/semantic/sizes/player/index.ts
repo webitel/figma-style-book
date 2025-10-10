f28a125993fbbf267196a4254d0aa0b399c430de
Олександр Пальонний <s2567k@gmail.com>
2025-10-10 19:55:39 +0300

    feat: remove player key from player object exports  [WTEL-7283](https://webitel.atlassian.net/browse/WTEL-7283)

--- src/lib/primevue/semantic/sizes/player/index.ts
+++ src/lib/primevue/semantic/sizes/player/index.ts
@@ -1,65 +1,63 @@
 export default {
-  player: {
-    counter: {
-      padding: '0.5rem',
-      gap: '0.5rem',
+  counter: {
+    padding: '0.5rem',
+    gap: '0.5rem',
+    borderRadius: '0.5rem',
+  },
+  camPreview: {
+    sm: {
       borderRadius: '0.5rem',
     },
-    camPreview: {
-      sm: {
-        borderRadius: '0.5rem',
-      },
-      md: {
-        borderRadius: '0.5rem',
-      },
-      lg: {
-        borderRadius: '0.5rem',
-      },
+    md: {
+      borderRadius: '0.5rem',
     },
-    wrapper: {
-      sm: {
-        borderRadius: '0.5rem',
-      },
-      md: {
-        borderRadius: '0.5rem',
-      },
-      lg: {
-        borderRadius: '0.5rem',
-      },
+    lg: {
+      borderRadius: '0.5rem',
     },
-    headline: {
-      sm: {
-        borderRadius: '0.5rem',
-        padding: '0.5rem',
-        gap: '0.5rem',
-      },
-      md: {
-        borderRadius: '0.5rem',
-        padding: '0.5rem',
-        gap: '0.5rem',
-      },
-      lg: {
-        borderRadius: '0.5rem',
-        padding: '0.5rem',
-        gap: '0.5rem',
-      },
+  },
+  wrapper: {
+    sm: {
+      borderRadius: '0.5rem',
     },
-    controlBar: {
-      sm: {
-        borderRadius: '0.5rem',
-        padding: '0.5rem',
-        gap: '0.5rem',
-      },
-      md: {
-        borderRadius: '0.5rem',
-        padding: '0.5rem',
-        gap: '0.5rem',
-      },
-      lg: {
-        borderRadius: '0.5rem',
-        padding: '0.5rem',
-        gap: '0.5rem',
-      },
+    md: {
+      borderRadius: '0.5rem',
     },
-  }
+    lg: {
+      borderRadius: '0.5rem',
+    },
+  },
+  headline: {
+    sm: {
+      borderRadius: '0.5rem',
+      padding: '0.5rem',
+      gap: '0.5rem',
+    },
+    md: {
+      borderRadius: '0.5rem',
+      padding: '0.5rem',
+      gap: '0.5rem',
+    },
+    lg: {
+      borderRadius: '0.5rem',
+      padding: '0.5rem',
+      gap: '0.5rem',
+    },
+  },
+  controlBar: {
+    sm: {
+      borderRadius: '0.5rem',
+      padding: '0.5rem',
+      gap: '0.5rem',
+    },
+    md: {
+      borderRadius: '0.5rem',
+      padding: '0.5rem',
+      gap: '0.5rem',
+    },
+    lg: {
+      borderRadius: '0.5rem',
+      padding: '0.5rem',
+      gap: '0.5rem',
+    },
+  },
 }

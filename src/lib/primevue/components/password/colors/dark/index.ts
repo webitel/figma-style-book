251b21a155b3e2aa38273ce4a1e243ef8ee406d7
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-11-13 13:57:59 +0200

    feat: add password component from primevue [WTEL-8083](https://webitel.atlassian.net/browse/WTEL-8083)

---
+++ src/lib/primevue/components/password/colors/dark/index.ts
@@ -0,0 +1,21 @@
+const darkColors = {
+  meter: {
+    background: '{content.border.color}'
+  },
+  icon: {
+    color: '{form.field.icon.color}'
+  },
+  overlay: {
+    background: '{overlay.popover.background}',
+    borderColor: '{overlay.popover.border.color}',
+    color: '{overlay.popover.color}',
+    shadow: '{overlay.popover.shadow}'
+  },
+  strength: {
+    weakBackground: '{red.400}',
+    mediumBackground: '{amber.400}',
+    strongBackground: '{green.400}'
+  }
+}
+
+export default darkColors

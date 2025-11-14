fadfd2177bfca7663fc9b20ebf3d2afe4fcbba60
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-11-14 09:18:47 +0200

    feat: add input-text and input-number components from primevue [WTEL-8083](https://webitel.atlassian.net/browse/WTEL-8083)

---
+++ src/lib/primevue/components/input-number/colors/light/index.ts
@@ -0,0 +1,15 @@
+const lightColors = {
+  button: {
+    background: 'transparent',
+    hoverBackground: '{surface.100}',
+    activeBackground: '{surface.200}',
+    borderColor: '{form.field.border.color}',
+    hoverBorderColor: '{form.field.border.color}',
+    activeBorderColor: '{form.field.border.color}',
+    color: '{surface.400}',
+    hoverColor: '{surface.500}',
+    activeColor: '{surface.600}'
+  }
+}
+
+export default lightColors

65a8dd976b78ef9e1db46dfe789e1b9e510704e2
Олександр Пальонний <s2567k@gmail.com>
2025-12-10 19:28:13 +0200

    feat: added badge styles [WTEL-8150](https://webitel.atlassian.net/browse/WTEL-8150)

---
+++ src/lib/primevue/components/badge/colors/dark/index.ts
@@ -0,0 +1,30 @@
+const darkColors = {
+  background: '{surface.100}',
+  color: '{surface.800}',
+  secondary: {
+    background: '{amber.650}',
+    color: '{amber.950}',
+  },
+  success: {
+    background: '{amber.650}',
+    color: '{amber.950}',
+  },
+  info: {
+    background: '{amber.650}',
+    color: '{amber.950}',
+  },
+  warn: {
+    background: '{amber.650}',
+    color: '{amber.950}',
+  },
+  danger: {
+    background: '{amber.650}',
+    color: '{amber.950}',
+  },
+  contrast: {
+    background: '{amber.650}',
+    color: '{amber.950}',
+  },
+}
+
+export default darkColors
